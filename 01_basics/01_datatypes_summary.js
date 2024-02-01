36f46744ccaad6925a41a4277c41c74909a1d720
Sumitcode19 <156463268+Sumitcode19@users.noreply.github.com>
2024-02-01 17:47:31 +0000

    summary on datatypes

---
+++ 01_basics/01_datatypes_summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BinInt, 
+
+const score = 100
+const scoreValue = 100.03
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 7835484574834938n
+
+
+
+//Refrence (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "sumit",
+    age: 19,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+
+}
+
+console.log(typeof anotherIdid);
