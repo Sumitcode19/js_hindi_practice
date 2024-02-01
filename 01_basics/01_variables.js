676a1d1d0181771f18d868d774a43eddfe48230f
Sumitcode19 <156463268+Sumitcode19@users.noreply.github.com>
2024-02-01 14:55:51 +0000

    conversionOperation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "sumitjindal@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Rajasthan"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and funticonal scope
+
+*/
+
+
+console.table([accountId, accountPassword, accountCity,accountEmail,accountState]);
+
